531471cb1f16fc8265cc027d9eae021f1092535d
Eamonn Sirhal <esirhal@g.syr.edu>
2024-12-13 22:07:31 +0000

    Fixing the dim_playlist files... I had some... issues.

--- CHINOOK/models/dim_playlist.sql
+++ CHINOOK/models/dim_playlist.sql
@@ -1,4 +1,15 @@
+{{ config(
+    materialized='table'  
+) }}
+
+WITH stg_playlists AS (
+    SELECT
+        PLAYLISTID AS playlist_id,   -- Ensure this alias matches the YAML
+        NAME AS playlist_name        -- Ensure this alias matches the YAML
+    FROM raw.chinook.playlist
+)
+
 SELECT
-    PLAYLISTID AS playlistid,
-    NAME AS playlistname
-FROM raw.chinook.playlist
+    {{ dbt_utils.generate_surrogate_key(['stg_playlists.playlist_id']) }} AS playlist_key, -- Surrogate key
+    stg_playlists.*
+FROM stg_playlists
