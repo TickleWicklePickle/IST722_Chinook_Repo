2db3689384f2ccc9b70612c998b7498d914190c2
Tibaria Alnouri <talnouri@syr.edu>
2024-12-16 03:40:03 +0000

    added date keys to all applicable dim and fact tables so that I'm not relying on power bi to recognize the date columns!!! >.<

--- CHINOOK/models/dim_invoices.sql
+++ CHINOOK/models/dim_invoices.sql
@@ -6,6 +6,7 @@ WITH stg_invoices AS (
     SELECT
         invoiceid AS invoice_id,            -- Primary key of the source system (business key)
         customerid AS customer_id,          -- Reference to the customer associated with the invoice
+        REPLACE(TO_DATE(invoicedate)::varchar,'-','')::int  as invoice_date_key, --date key
         to_date(invoicedate) AS invoice_date,        -- Date of the invoice
         billingaddress AS billing_address,  -- Billing address
         billingcity AS billing_city,        -- Billing city
